bdf566a7fdca4936f8c808fedac0a6c629e8933f
Slava <slava91petrushin@yandex.ru>
2019-10-15 19:56:45 +0300

    no work static

--- routers/index.js
+++ routers/index.js
@@ -31,7 +31,7 @@ router.get('/', async (ctx) =>  {
 				products,
 				skills,
 				social,
-				msgsemail
+				//msgsemail
 			});			
 		})
 		.catch(error => ctx.render('error', {message: error.message}))
